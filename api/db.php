09f9842a4c78bc1c241f3bd06ec84b02458b7ecc
Aldair Martinez <martinezlunaaldair325@gmail.com>
2024-09-11 17:30:52 -0600

    db comm

--- api/db.php
+++ api/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "srv992.hstgr.io";
-$db = "u663124265_vje";
-$user = "u663124265_vje";
-$pass = "9k:9zv1ISD+C";
+$host = "";
+$db = "";
+$user = "";
+$pass = "";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
